44d2d97a452cbbad3426cd093fc1d653ea5fca4c
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-11-14 16:44:32 +0100

    creata tab con task eliminabile e taggabile "done"

--- main.js
+++ main.js
@@ -2,12 +2,44 @@ var app = new Vue(
     {
         el: '#root',
         data: {
+            task: [
+                {
+                    text: 'comprare una giraffa',
+                    done: false
+                },
+                {
+                    text: 'capire come cucinare giraffe',
+                    done: false
+                },
+                {
+                    text: 'portare fuori spazzatura',
+                    done: false
+                },
+                {
+                    text: 'trovare avvocato animalista',
+                    done: false
+                },
+            ],
 
+            completedTask: [
+
+            ]
         },
 
         methods: {
+
+            removeElement(element, index){
+                this.task.splice(index, 1);
+            },
+
+            completed(element, index){
+                this.completedTask.push(element);
+                this.task.splice(index, 1);
+            }
 
         }
 
     }
 )
+
+
